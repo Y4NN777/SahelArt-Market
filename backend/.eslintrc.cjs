c80eadfcef84ef61acf6015a2df81eafcde015fe
Y4NN777 <axel.studiesmail@gmail.com>
2026-02-11 02:07:39 +0000

    chore(dev): add .gitignore and fix eslint unused-vars config

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_', ignoreRestSiblings: true }]
   }
 };
